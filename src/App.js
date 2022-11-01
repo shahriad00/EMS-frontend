8d6544774708748d2f749d8f324f7b276c78ae4d
shahriad00 <shahriad555@gmail.com>
2022-11-01 22:01:48 +0600

    login page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import AppLayout from './Layout/Layout';
+import Login from './Pages/Login';
 
 const App = () => {
   return (
     <>
-      <AppLayout/>
+    <Login/>
+      {/* <AppLayout/> */}
     </>
   );
 };
