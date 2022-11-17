4d9a048b16fe7508f7956266cba16b00790fa8f2
shahriad00 <shahriad555@gmail.com>
2022-11-17 20:40:56 +0600

    leave delete added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,13 @@
-import { FileDoneOutlined, FundProjectionScreenOutlined, SolutionOutlined } from "@ant-design/icons";
-import React from "react";
+import {
+    FileDoneOutlined,
+    FundProjectionScreenOutlined,
+    SolutionOutlined
+} from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
 import { FaUserFriends } from "react-icons/fa";
+import { toast } from "react-toastify";
 import CalendarComp from "../../Components/Calender/CalenderComp";
+import axiosInstance from "../../services/axiosInstance";
 
 const style = {
     color: "white",
@@ -9,32 +15,74 @@ const style = {
 };
 
 const Dashboard = () => {
+    const [allProject, setAllProject] = useState("");
+    const [allEmployees, setAllEmployees] = useState("");
+    const [allLeaves, setAllLeaves] = useState("");
+
+    useEffect(() => {
+        let isMounted = true;
+        if (isMounted) {
+            axiosInstance
+                .get(`/api/all-project`)
+                .then((res) => setAllProject(res.data))
+                .catch((err) => {
+                    toast.error("Something went wrong while Loading");
+                });
+            axiosInstance
+                .get(`/api/all-employees`)
+                .then((res) => setAllEmployees(res.data))
+                .catch((err) => {
+                    toast.error("Something went wrong while Loading");
+                });
+            axiosInstance
+                .get(`/api/all-applied-leave-applications`)
+                .then((res) => setAllLeaves(res.data))             
+                .catch((err) => {
+                    toast.error("Something went wrong while Loading");
+                });
+        }
+        return () => {
+            isMounted = false;
+        };
+    }, []);
     return (
         <>
             <div className="px-8 py-12">
                 <div className="flex justify-between">
-                    <Card bgColor="bg-[#3499ef]" number="15" text="Total Projects">
+                    <Card
+                        bgColor="bg-[#3499ef]"
+                        number={allProject.length}
+                        text="Total Projects"
+                    >
                         <FundProjectionScreenOutlined style={style} />
                     </Card>
-                    <Card bgColor="bg-[#52ac56]" number="3" text="Leave applications">
+                    <Card
+                        bgColor="bg-[#52ac56]"
+                        number={allLeaves.length}
+                        text="Leave applications"
+                    >
                         <FileDoneOutlined style={style} />
                     </Card>
                     <Card bgColor="bg-[#fc9107]" number="2" text="On Leave">
-                        <SolutionOutlined  style={style} />
+                        <SolutionOutlined style={style} />
                     </Card>
-                    <Card bgColor="bg-[#e12c6c]" number="10" text="Total Employee">
+                    <Card
+                        bgColor="bg-[#e12c6c]"
+                        number={allEmployees.length}
+                        text="Total Employee"
+                    >
                         <FaUserFriends style={style} />
                     </Card>
                 </div>
                 <div className="shadow rounded-xl my-5">
-                    <CalendarComp/>
+                    <CalendarComp />
                 </div>
             </div>
         </>
     );
 };
 
-const Card = ({ bgColor, children,number, text }) => {
+const Card = ({ bgColor, children, number, text }) => {
     return (
         <>
             <div className="w-[230px] bg-white rounded-lg relative shadow pl-10 pt-10 pr-5 pb-5">
--- src/Pages/LeaveApplication/LeaveApplication.js
+++ src/Pages/LeaveApplication/LeaveApplication.js
@@ -1,6 +1,11 @@
-import { ClockCircleTwoTone, EyeOutlined } from "@ant-design/icons";
+import {
+    ClockCircleTwoTone,
+    DeleteOutlined,
+    EyeOutlined
+} from "@ant-design/icons";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import axiosInstance from "../../services/axiosInstance";
 
 const LeaveApplication = () => {
@@ -23,6 +28,21 @@ const LeaveApplication = () => {
         };
     }, []);
 
+    const handleDeleteLeave = (id) => {
+        axiosInstance
+            .delete(`/api/leave-application/${id}`)
+            .then((res) => {
+                toast.success(res.data.message);
+                axiosInstance
+                    .get(`/api/all-applied-leave-applications`)
+                    .then((res) => {
+                        setLeaves(res.data);
+                    })
+                    .catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
+    };
+
     return (
         <div className="m-4 p-4 bg-white rounded">
             <div>
@@ -33,22 +53,50 @@ const LeaveApplication = () => {
             <div className="my-6">
                 {leaves?.map((leave, i) => (
                     <div
-                        key = {leave._id}
-                        onClick={() => navigate(`/leave-application-details/${leave._id}`)}
+                        key={leave._id}
                         className="flex items-center justify-between gap-4 border border-sky-600 text-md font-medium rounded-lg shadow hover:shadow-lg  hover:cursor-pointer p-4 mb-4"
                     >
                         <span className="flex items-center gap-4">
-                            <ClockCircleTwoTone /> {i+1}. {leave?.name} - [{leave?.type}]...{" "}
-                
+                            <ClockCircleTwoTone /> {i + 1}. {leave?.name} - [
+                            {leave?.type}]{" "}
                         </span>
-                        <span className="text-gray-500 font-small">Apply Date: {leave.appliedDate.slice(0, 15)}</span>
+                        <span className="text-gray-500 font-small">
+                            Apply Date: {leave.appliedDate.slice(0, 15)}
+                        </span>
                         <div className="flex items-center justify-center gap-5">
-                            {leave.adminResponse === 'Accepted' && <span className="text-green-600">{leave.adminResponse}</span>}
-                            {leave.adminResponse === 'Rejected' && <span className="text-red-600">{leave.adminResponse}</span>}
-                            {leave.adminResponse === 'pending' && <span className="text-yellow-600">{leave.adminResponse}</span>}
-                            <span className="flex items-center justify-center p-2 rounded-full bg-gray-100 hover:bg-gray-200">
+                            {leave.adminResponse === "Accepted" && (
+                                <span className="text-green-600">
+                                    {leave.adminResponse}
+                                </span>
+                            )}
+                            {leave.adminResponse === "Rejected" && (
+                                <span className="text-red-600">
+                                    {leave.adminResponse}
+                                </span>
+                            )}
+                            {leave.adminResponse === "pending" && (
+                                <span className="text-yellow-600">
+                                    {leave.adminResponse}
+                                </span>
+                            )}
+                            <span
+                                title="view"
+                                className="flex items-center justify-center p-2 rounded-full bg-gray-100 hover:bg-gray-200"
+                                onClick={() =>
+                                    navigate(`/leave-application-details/${leave._id}`)
+                                }
+                            >
                                 <EyeOutlined
                                     style={{ fontSize: "20px", color: "black" }}
+                                />
+                            </span>
+                            <span
+                                title="delete"
+                                className="flex items-center justify-center p-2 rounded-full bg-gray-100 hover:bg-gray-200"
+                                onClick={() => handleDeleteLeave(leave._id)}
+                            >
+                                <DeleteOutlined
+                                    style={{ fontSize: "20px", color: "red" }}
                                 />
                             </span>
                         </div>
