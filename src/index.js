0aa28ba6618c22e57031ebde1bd0978dcd2abc75
shahriad00 <shahriad555@gmail.com>
2023-05-19 02:05:04 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
+import 'antd/dist/antd.min.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
