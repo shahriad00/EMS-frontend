6c93bc15bead2e3c0c30cb5ab6487203458ec87a
shahriad00 <shahriad555@gmail.com>
2023-05-19 01:03:52 +0600

    baseurl added

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
 const axiosInstance = Axios.create({
-  baseURL: "http://localhost:8000/",
+  // baseURL: "http://localhost:8000/",
+  baseURL: "https://ems-backend-gnh6.onrender.com",
   withCredentials: false,
   "Content-Type": "application/json",
 });
